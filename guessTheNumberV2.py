10acfac4757d635b1a9817e9ab12bab888b5c2b6
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-03-08 14:17:02 +0600

    Add attempt tracking and update input prompt in number guessing game

--- guessTheNumberV2.py
+++ guessTheNumberV2.py
@@ -4,14 +4,17 @@ def main():
     computer = randint(1, 100)
     player = int(input('Enter a number between 1 and 10: '))
 
+    guesses = 0
     while player != computer:
         if player > computer:
             print("Lower number please! ")
+            guesses += 1
         elif player < computer:
             print("Higher number please! ")
-        player = int(input())
+            guesses += 1
+        player = int(input('Enter a number between 1 and 100: '))
     else:
-        print("You guessed it right!")
+        print("You guessed it right in {} attempts!".format(guesses))
 
 if __name__ == "__main__":
     main()
