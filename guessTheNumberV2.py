27da9c2c8b69621595163a1261a763290da49738
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2025-03-08 06:36:05 +0600

    Refactor guessTheNumberV2.py to improve game logic and expand number range

--- guessTheNumberV2.py
+++ guessTheNumberV2.py
@@ -1,14 +1,17 @@
 from random import randint
 
-computer = randint(1, 10)
+def main():
+    computer = randint(1, 100)
+    player = int(input('Enter a number between 1 and 10: '))
 
-player = int(input('Enter a number between 1 and 10: '))
+    while player != computer:
+        if player > computer:
+            print("Lower number please! ")
+        elif player < computer:
+            print("Higher number please! ")
+        player = int(input())
+    else:
+        print("You guessed it right!")
 
-while player != computer:
-    if player > computer:
-        print("Lower number please! ")
-    elif player < computer:
-        print("Higher number please! ")
-    player = int(input())
-else:
-    print("You guessed it right!")
+if __name__ == "__main__":
+    main()
